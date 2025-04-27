21d3ebb1699d2c5a14a07044c3f24bc53d70f231
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-04-27 13:15:48 -0700

    Add utility functions to juliana theme
    Signed-off-by: Joseph Britton <85815005+JosBritton@users.noreply.github.com>

--- colors/juliana.lua
+++ colors/juliana.lua
@@ -39,6 +39,47 @@ local M = {
     },
 }
 
+---@param hex_str string
+---@return number[]
+function M.hexToRgb(hex_str)
+    local hex = "[abcdef0-9][abcdef0-9]"
+    local pat = "^#(" .. hex .. ")(" .. hex .. ")(" .. hex .. ")$"
+    hex_str = string.lower(hex_str)
+
+    assert(
+        string.find(hex_str, pat) ~= nil,
+        "hex_to_rgb: invalid hex_str: " .. tostring(hex_str)
+    )
+
+    local r, g, b = string.match(hex_str, pat)
+    return { tonumber(r, 16), tonumber(g, 16), tonumber(b, 16) }
+end
+
+---@param fg string foreground color
+---@param bg string background color
+---@param alpha number number between 0 and 1. 0 results in bg, 1 results in fg
+---@return string
+function M.blend(fg, bg, alpha)
+    local bg = M.hexToRgb(bg)
+    local fg = M.hexToRgb(fg)
+
+    local blendChannel = function(i)
+        local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
+        return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+    end
+
+    return string.format(
+        "#%02X%02X%02X",
+        blendChannel(1),
+        blendChannel(2),
+        blendChannel(3)
+    )
+end
+
+function M.darken(hex, amount, bg)
+    return M.blend(hex, bg or "#000000", math.abs(amount))
+end
+
 ---@return nil
 function M:load()
     if vim.g.colors_name then
