53e8e0866a643d0ece10b1ba32b2251a116bb134
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-05-06 12:21:57 -0700

    Add note regarding blink LSP capability extension

--- lua/josbritton/plugins/lspconfig.lua
+++ lua/josbritton/plugins/lspconfig.lua
@@ -318,7 +318,7 @@ return {
         capabilities = vim.tbl_deep_extend(
             "force",
             capabilities,
-            require("blink-cmp").get_lsp_capabilities()
+            require("blink-cmp").get_lsp_capabilities() -- additional capabilities from blink
         )
 
         ---@type table<string, vim.lsp.Config>
