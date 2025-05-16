173ebc640e069ed0e92df8bf58d00f822f0c4d83
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-05-16 05:23:03 -0700

    Cleanup old commented code blocks from keymaps

--- lua/josbritton/keymaps.lua
+++ lua/josbritton/keymaps.lua
@@ -22,11 +22,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
 })
 
--- vim.keymap.set("n", "<leader>pv",
---     vim.cmd.Ex,
---     { desc = "Open Netrw" }
--- )
-
 vim.keymap.set(
     { "c", "i" },
     "<C-BS>",
@@ -42,15 +37,10 @@ vim.keymap.set(
     { noremap = true, desc = "Clear highlighting" }
 )
 
---vim.keymap.set("n", "cy", ""*y", { desc = "Yank to OS clipboard" })
---vim.keymap.set("n", "cp", ""*p", { desc = "Paste from OS clipboard" })
-
 -- center buffer on search result navigation
 vim.keymap.set("n", "n", "nzzzv", { noremap = true })
 vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
 
--- vim.keymap.set("n", "n", "nzz", { noremap = true })
--- vim.keymap.set("n", "N", "Nzz", { noremap = true })
 vim.keymap.set("n", "*", "*zz", { noremap = true })
 vim.keymap.set("n", "#", "#zz", { noremap = true })
 vim.keymap.set("n", "g*", "g*zz", { noremap = true })
@@ -106,18 +96,11 @@ vim.keymap.set(
     { noremap = true, desc = "Move line up" }
 )
 
--- vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "Paste to void reg" })
-
 vim.keymap.set("n", "Q", "<Nop>")
 
 vim.keymap.set("n", "<C-f>", "<Cmd>silent !tmux neww switchproj<CR>")
 vim.keymap.set("n", "<C-j>", "<Cmd>silent !tmux neww tmuxopensesh<CR>")
 vim.keymap.set("n", "<C-Space>", "<Cmd>silent !scratchtmux<CR>")
-
--- vim.keymap.set("n", "<C-k>", "<Cmd>cnext<CR>zz", { desc = "Quickfix nav next"})
--- vim.keymap.set("n", "<C-j>", "<Cmd>cprev<CR>zz", { desc = "Quickfix nav previous"})
--- vim.keymap.set("n", "<leader>k", "<Cmd>lnext<CR>zz", { desc = "Location list nav next"})
--- vim.keymap.set("n", "<leader>j", "<Cmd>lprev<CR>zz", { desc = "Location list nav previous"})
 
 vim.keymap.set("n", "<leader>S", [[:%s/]], { desc = "Enter substitute command" })
 vim.keymap.set("x", "<leader>S", [[:s/]], { desc = "Enter substitute command" })
