309adb901c06763fb0af8d66098811606b525278
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-04-11 03:49:16 -0700

    Reconfigure yaml indentation default

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,4 +1,4 @@
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
 vim.api.nvim_set_option_value("indentexpr", nil, { scope = "local" })
