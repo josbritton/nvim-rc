80efa193e4d943b8c5364e27faa0a5a8dfa79091
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-04-11 03:53:51 -0700

    Remove markdownpreview

--- lua/josbritton/plugins/markdownpreview.lua
+++
@@ -1,9 +0,0 @@
----@type LazyPluginSpec
-return {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && yarn install",
-    ft = "markdown",
-    cond = function()
-        return vim.fn.executable("yarn") == 1
-    end,
-}
