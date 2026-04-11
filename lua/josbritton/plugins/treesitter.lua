4ae2bdb594059a4765c726de06210d6f32fbb43c
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-04-11 03:45:24 -0700

    Implement automatic install functionality for tree-sitter parsers

--- lua/josbritton/plugins/treesitter.lua
+++ lua/josbritton/plugins/treesitter.lua
@@ -1,3 +1,35 @@
+local ts, available_langs = nil, nil
+
+local gid = vim.api.nvim_create_augroup("TreesitterAutoInstall", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = gid,
+    callback = function(ev)
+        ts = require("nvim-treesitter")
+
+        local lang = vim.treesitter.language.get_lang(ev.match)
+        if not lang then
+            return
+        end
+
+        available_langs = available_langs or ts.get_available()
+        if not vim.tbl_contains(available_langs, lang) then
+            return
+        end
+
+        local start = function()
+            if vim.api.nvim_buf_is_valid(ev.buf) then
+                vim.treesitter.start(ev.buf)
+            end
+        end
+
+        if vim.tbl_contains(ts.get_installed(), lang) then
+            start()
+        else
+            ts.install(lang):await(start)
+        end
+    end,
+})
+
 return {
     ---@type LazyPluginSpec
     {
