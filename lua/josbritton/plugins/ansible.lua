1be83737a2d6019c7772e17ac53f6b931ab7fd9d
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-06-05 22:48:16 -0700

    Update ansible setup logic for use with the nvim-treesitter rewrite

--- lua/josbritton/plugins/ansible.lua
+++ lua/josbritton/plugins/ansible.lua
@@ -1,6 +1,18 @@
 ---@type LazyPluginSpec
 return {
     "pearofducks/ansible-vim",
-    ft = { "yaml.ansible", "jinja2" },
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
+    ft = { "ansible", "jinja2" },
     lazy = true,
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        local t = vim.tbl_filter(function(lang)
+            return not vim.tbl_contains(ts.get_installed(), lang)
+        end, { "jinja", "yaml" })
+
+        ts.install(t):await(require("ansible").setup)
+    end,
 }
