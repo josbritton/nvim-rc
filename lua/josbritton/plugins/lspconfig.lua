ea592023a597bffb3bbf28abe24bbf91cced99f1
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-06-22 06:02:50 -0700

    Use ansible filetype for ansiblels matching instead of yaml.ansible As used by ansible-vim since 024274576de77db723eb1f0478da046fc5f1507f

--- lua/josbritton/plugins/lspconfig.lua
+++ lua/josbritton/plugins/lspconfig.lua
@@ -586,6 +586,9 @@ return {
                     },
                 },
             },
+            ansiblels = {
+                filetypes = { "ansible" },
+            },
         }
 
         require("mason").setup({
