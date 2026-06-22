4900c1900c866c376a576086f2d997ff98b4372d
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-06-22 06:02:26 -0700

    Add type annotation to lazydev opts

--- lua/josbritton/plugins/lazydev.lua
+++ lua/josbritton/plugins/lazydev.lua
@@ -2,9 +2,11 @@
 return {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
+    ---@type lazydev.Config
     opts = {
         library = {
             "lazy.nvim",
+            "lazydev.nvim",
             -- See the configuration section for more details
             -- Load luvit types when the `vim.uv` word is found
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
