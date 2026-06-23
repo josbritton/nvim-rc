98b6ce0d002012fe1882ef0f3d48ace4e12a1cc9
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-06-22 19:32:37 -0700

    Add preliminary markdown inline rendering for lsp hold/keywordprg popups

---
+++ lua/josbritton/plugins/render-markdown.lua
@@ -0,0 +1,39 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter", "nvim-web-devicons" },
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        local t = vim.tbl_filter(function(lang)
+            return not vim.tbl_contains(ts.get_installed(), lang)
+        end, { "markdown", "markdown_inline", "html", "latex", "yaml" })
+
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        local opts = {
+            html = {
+                tag = {
+                    code = {
+                        scope_highlight = "RenderMarkdownCode",
+                    },
+                    strong = {
+                        scope_highlight = "htmlBold",
+                    },
+                    em = {
+                        scope_highlight = "@text.emphasis",
+                    },
+                },
+            },
+            code = {
+                language = false,
+                disable_background = true,
+            },
+        }
+
+        local setup = function()
+            require("render-markdown").setup(opts)
+        end
+        ts.install(t):await(setup)
+    end,
+    opts = {},
+}
